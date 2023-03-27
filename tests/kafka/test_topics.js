791ca7f004cdeeb89b6b3e4905dbf960ec7a008f
Andrey Shabunov <shabunovaa@yandex.ru>
2023-03-27 22:14:14 +0300

    27.03.2023 add selenoid directory

--- tests/kafka/test_topics.js
+++ tests/kafka/test_topics.js
@@ -1,14 +1,7 @@
-/*
-This is a k6 test script that imports the xk6-kafka and
-list topics on all Kafka partitions and creates a topic.
-*/
-
 import { check } from "k6";
-import { Connection } from "k6/x/kafka"; // import kafka extension
+import { Connection } from "k6/x/kafka";
 
 const address = "localhost:9092";
-// const randomNumber = Math.floor(Math.random() * 1000);
-// const topic = "xk6_kafka_test_topic" + `${randomNumber}`;
 const topic = "xk6_kafka_test_topic";
 
 const connection = new Connection({
@@ -28,7 +21,6 @@ export default function () {
 
 export function teardown(data) {
     if (__VU == 0) {
-        // Delete the topic
         connection.deleteTopic(topic);
     }
     const results2 = connection.listTopics();
